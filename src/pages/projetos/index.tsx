689b3a9182792dd3611d36060431255f3d716149
Monalisa Menezes <psimonafmenezes@gmail.com>
2022-06-25 15:56:07 -0300

    feat: create contacts page

---
+++ src/pages/projetos/index.tsx
@@ -0,0 +1,16 @@
+import { ContainerProjetos } from "./styles"
+import btcurrent from '../../../public/images/btcurrent.png'
+import Image from "next/image";
+
+
+const Projetos = () => {
+
+  
+    return(
+        <ContainerProjetos>
+            <h1>{'<Projetos'} <span>/</span>{'>'} 💻</h1>
+        </ContainerProjetos>
+    )
+}
+
+export default Projetos
---
+++ src/pages/projetos/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from "@mui/material";
+
+export const ContainerProjetos = styled("main")`
+  height: 75%;
+  background-color: #b5838d;
+  padding: 0 3rem;
+`;
